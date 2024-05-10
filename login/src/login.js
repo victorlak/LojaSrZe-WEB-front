a270456337755171f6928dac0b8b22200c8171ae
victorlak <victormirandadasilvasouza9999@gmail.com>
2024-05-10 16:18:02 -0300

    segundo

--- login/src/login.js
+++ login/src/login.js
@@ -1,7 +1,27 @@
-import { Button } from 'bootstrap';
+import { Button} from 'bootstrap';
+import { useState } from 'react';
 import './login.css';
 
 function Login() {
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+
+    const handleChangeEmail= (e) => {
+        setEmail(e.target.value)
+    }
+    const handleChangeSenha= (e) => {
+        setSenha(e.target.value)
+    }
+    function criarObjeto(){
+        const login = {
+            Email: email,
+            Senha: senha
+        }
+        console.log(login);
+        return login
+    }
+
+
   return (
     <div className="All">
         <div className='Login'>
@@ -9,14 +29,14 @@ function Login() {
                 <div class="mb-3 row">
                     <h1 className='display-2'>Login:</h1>
                     <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-                    <input type="text" class="form-control"></input>
+                    <input onChange={handleChangeEmail} type="text" class="form-control"></input>
                 </div>
                 <div class="mb-3 row">
                     <label for="inputPassword" class="col-sm-2 col-form-label">Senha</label>
-                        <input type="password" class="form-control" id="inputPassword"></input>
+                        <input onChange={handleChangeSenha} type="password" class="form-control" id="inputPassword"></input>
                         <div className='LoginCadastro'>
                             <button style={{color:'white'}} type="button" class="btn btn-warning">Cadastrar</button>
-                            <button style={{color:'white'}} type="button" class="btn btn-warning">Entrar</button>
+                            <button onClick={criarObjeto} style={{color:'white'}} type="button" class="btn btn-warning">Entrar</button>
                         </div>
                 </div>
             </div>
